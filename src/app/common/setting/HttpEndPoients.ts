53f0384da4f799bba1d7a81d5fa5f9df7464e7a5
Sara-Elsherbini <sarasherbini98@gmail.com>
2024-05-28 19:15:44 +0300

    added endPoient

--- src/app/common/setting/HttpEndPoients.ts
+++ src/app/common/setting/HttpEndPoients.ts
@@ -1,5 +1,9 @@
 export class HttpEndPoints {
   public static Auth = {
+   login:'portal/users/login',
+   register:'portal/users',
+   forgetPass:'portal/users/forgot-password',
+   resetPass:'portal/users/reset-password'
 
   }
 }
