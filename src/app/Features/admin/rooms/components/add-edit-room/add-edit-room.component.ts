74b8cd120a4261864755de06a5b91aa691e5f113
Sara-Elsherbini <sarasherbini98@gmail.com>
2024-05-31 13:38:39 +0300

    Added add-edit-room

---
+++ src/app/Features/admin/rooms/components/add-edit-room/add-edit-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-room',
+  templateUrl: './add-edit-room.component.html',
+  styleUrls: ['./add-edit-room.component.css']
+})
+export class AddEditRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
