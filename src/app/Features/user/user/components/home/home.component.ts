68a43e6407db9a3bb6e208905652a59e97a4b71b
Sara-Elsherbini <sarasherbini98@gmail.com>
2024-06-08 10:12:21 +0300

    Added section2( landing page)

--- src/app/Features/user/user/components/home/home.component.ts
+++ src/app/Features/user/user/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import {  Ads} from '../../../../admin/ads/model/Ads'
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotifyService } from 'src/app/common';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  capacity:number=0;
+  adsList: Ads.IAds[]=[];
+  firstAds!:Ads.IAds
+  constructor(private _UserService:UserService,private _NotifyService:NotifyService) { }
 
   ngOnInit() {
+    this.getAds()
+  }
+
+  increment(){
+    this.capacity++;
+  }
+  decrement(){
+    if (this.capacity>0) {
+      this.capacity--;
+    }
+
+  }
+
+  getAds() {
+    this._UserService.getAllAds().subscribe({
+      next: (res) => {
+        this.firstAds=res.data.ads[0];
+        this.adsList = res.data.ads.slice(1, 5);
+        console.log('adsList',this.adsList);
+
+      },
+      error: (err:HttpErrorResponse) => {
+        this._NotifyService.ServerError(err.error.message)
+      }
+    })
   }
 
 }
--- src/app/Features/user/user/services/user.service.ts
+++ src/app/Features/user/user/services/user.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpEndPoints } from 'src/app/common/setting/HttpEndPoients';
+import { Ads } from '../../../admin/ads/model/Ads';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-constructor() { }
-
+  constructor(private _HttpClient: HttpClient) { }
+  getAllAds(): Observable<Ads.IResponseMulti> {
+    return this._HttpClient.get<Ads.IResponseMulti>(HttpEndPoints.User.home.GetAllads)
+  }
 }
